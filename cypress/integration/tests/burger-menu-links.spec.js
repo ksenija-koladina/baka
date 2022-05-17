986de094b43da725bfadaa7513f6c7d4db9e1ec0
Ksenia Kolyadina <ksenija.koladina@gmail.com>
2022-05-17 12:59:07 +0300

    burger menu links file  modiication

--- cypress/integration/tests/burger-menu-links.spec.js
+++ cypress/integration/tests/burger-menu-links.spec.js
@@ -1,14 +1,17 @@
 /// <reference types="cypress" />
 
-describe('Burger izvēlne - aizvērt', () => {
+describe('Burger izvēlnes saites', () => {
   beforeEach(() => {
-    cy.visit('mājaslapas adrese'); // ietvars apmeklē mājaslapu
-    cy.get('burger izvēlne').click() // ietvars klikšķina uz burger menu un atvēr to
+    cy.visit('https://liepu.lv/lv'); // ietvars apmeklē mājaslapu
+    cy.get('body > header > a').click() // ietvars klikšķina uz burger menu un atvēr to
+    cy.get('#menu > div').should('be.visible'); // burger izvēlnes logs ir redzams
   })
 
-  it('Burger menu aizvērās ar krustiņa klikšķi', () => {
-      cy.get('krustiņš').click(); // ietvars klikšķina uz burger izvēlnes krustiņu (close poga) un aizvēr to
-      cy.get('burger izvēlnes logs').should('not.be.visible'); // burger izvēlnes logs nav redzams
+  it('Burger izvēlnes saites atvēr vajadzīgo lapu un saturs atbilst saitei', () => {
+      cy.get('#menu > div > ul.shortcuts').should('be.visible'); // izvēlnē rādas saraksta elementi
+      cy.get('#menu > div > ul.shortcuts > li:nth-child(1) > a').click(); //klikšķis uz elementu 
+      cy.url().should('include', '/projekti'); //saites pārbaude (vai notika navigācija)
+      cy.get(':nth-child(1) > .head > .title').should('contain.text', 'Projekti'); //virsraksta pārbaude
   })
 
 
