8676fc642e0de729fea75439feab6e685422a0af
Ksenia Kolyadina <ksenija.koladina@gmail.com>
2022-05-24 21:59:56 +0300

    testing autorization success

--- cypress/integration/tests/aut-success.spec.js
+++ cypress/integration/tests/aut-success.spec.js
@@ -2,19 +2,19 @@
 
 describe('Autorizēšanas formas testēšana - veiksmīga', () => {
   beforeEach(() => { //cikls, kas atkārtojas pirms katrā testā
-      cy.visit('mājaslapas adrese'); // ietvars atver mājaslapu
-      cy.get('Sign in poga').should('exist').click(); //ietvars atrod ielogošanas pogu uz klikšķina uz to
-      cy.url().should('include', '/loginpage url artibūts'); // ietvars pārbaude linka korektumu
-      cy.get('login lauks').should('exist'); // ietvars pārbauda lauka login esamĪbu
-      cy.get('paroles lauks').should('exist'); // ietvars pārbauda lauka parole esamĪbu
-      cy.get('log in poga').should('exist'); // ietvars pārbauda login pogas esamĪbu
+      cy.visit('https://estudijas.liepu.lv/'); // ietvars atver mājaslapu
+      // lieks solis šajā sistēma - cy.get('#login > div:nth-child(4) > input').should('exist').click(); //ietvars atrod ielogošanas pogu uz klikšķina uz to
+      // lieks solis šajā sistēma - cy.url().should('include', '/loginpage url artibūts'); // ietvars pārbaude linka korektumu
+      cy.get('#login_username').should('exist'); // ietvars pārbauda lauka login esamĪbu
+      cy.get('#login_password').should('exist'); // ietvars pārbauda lauka parole esamĪbu
+      cy.get('#login > :nth-child(4) > .btn').should('exist'); // ietvars pārbauda login pogas esamĪbu
   })
 
   it('Login formas aizpildīšana', () => {
-      cy.get('login lauks').click().type('login'); //ietvars aizpilda login lauku ar loginu
-      cy.get('paroles lauks').click().type('parole'); //ietvars aizpilda paroles lauku ar paroli
-      cy.get('log in poga').should('exist').click(); ////ietvars click "Log in"
-      cy.url().should('include', '/lietotāja profils'); //lietotāja profila url pārbaude
+      cy.get('#login_username').click().type('login'); //ietvars aizpilda login lauku ar loginu
+      cy.get('#login_password').click().type('parole'); //ietvars aizpilda paroles lauku ar paroli
+      cy.get('#login > :nth-child(4) > .btn').should('exist').click(); ////ietvars click "Log in"
+      cy.url().should('include', '/my/'); //lietotāja profila url pārbaude
   })
 
 })
