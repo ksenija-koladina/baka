be29012826f9e2f9eed487c21b27f6ca648d1444
Ksenia Kolyadina <ksenija.koladina@gmail.com>
2022-05-24 21:05:05 +0300

    dropdown fix

--- cypress/integration/tests/dropdown.spec.js
+++ cypress/integration/tests/dropdown.spec.js
@@ -3,15 +3,15 @@
 describe('Dropdown izvēlne', () => {
   beforeEach(() => {
     cy.visit('https://liepu.lv/lv'); // ietvars apmeklē mājaslapu
-    cy.get('body > header > a').click() // ietvars klikšķina uz burger menu un atvēr to (papildus solis)
+    cy.get('body > header > a').click() // ietvars klikšķina uz burger izvēlnes un atver to (papildus solis)
     cy.get('#menu > div').should('be.visible'); // burger izvēlnes logs ir redzams (papildus solis)
   })
 
-  it('Burger izvēlnes saites atvēr vajadzīgo lapu un saturs atbilst saitei', () => {
-      cy.get('#menu > div > ul.menu > li:nth-child(1) > a').should('be.visible'); // ietvars pārbauda dropdown izvelni
+  it('Dropdown izvēlnes saites atvēr vajadzīgo lapu un saturs atbilst saitei', () => {
+      cy.get('#menu > div > ul.menu > li:nth-child(1) > a').should('be.visible'); // ietvars pārbauda dropdown izvēlni
       cy.get('#menu > div > ul.menu > li:nth-child(1) > a').click(); //ietvars klikšķina uz dropdown izvēlni 
-      cy.get('#menu > div > ul.menu > li:nth-child(1) > ul > li:nth-child(2) > a').should('be.visible'); //izvelnes saraksta elementi ir redzami
-      cy.get('#menu > div > ul.menu > li:nth-child(1) > ul > li:nth-child(2) > a').click(); //ietvars klikšķina uz dropdown izvelnes saraksta elementu
+      cy.get('#menu > div > ul.menu > li:nth-child(1) > ul > li:nth-child(2) > a').should('be.visible'); //izvēlnes saraksta elementi ir redzami
+      cy.get('#menu > div > ul.menu > li:nth-child(1) > ul > li:nth-child(2) > a').click(); //ietvars klikšķina uz dropdown izvēlnes saraksta elementu
       cy.url().should('include', '/studiju-programmas'); //saites pārbaude (vai notika navigācija)
       cy.get(':nth-child(1) > .head > .title').should('contain.text', 'Pamatstudijas'); //virsraksta pārbaude
   })
