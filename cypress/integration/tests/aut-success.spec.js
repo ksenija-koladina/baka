702e5b42f824aedbe3e6e29c873a246f60506bbf
Ksenia Kolyadina <ksenija.koladina@gmail.com>
2022-05-25 01:46:12 +0300

    aut success fix

--- cypress/integration/tests/aut-success.spec.js
+++ cypress/integration/tests/aut-success.spec.js
@@ -1,20 +1,20 @@
 /// <reference types="cypress" />
 
 describe('Autorizēšanas formas testēšana - veiksmīga', () => {
-  beforeEach(() => { //cikls, kas atkārtojas pirms katrā testā
-      cy.visit('https://estudijas.liepu.lv/'); // ietvars atver mājaslapu
-      // lieks solis šajā sistēma - cy.get('#login > div:nth-child(4) > input').should('exist').click(); //ietvars atrod ielogošanas pogu uz klikšķina uz to
-      // lieks solis šajā sistēma - cy.url().should('include', '/loginpage url artibūts'); // ietvars pārbaude linka korektumu
-      cy.get('#login_username').should('exist'); // ietvars pārbauda lauka login esamĪbu
-      cy.get('#login_password').should('exist'); // ietvars pārbauda lauka parole esamĪbu
-      cy.get('#login > :nth-child(4) > .btn').should('exist'); // ietvars pārbauda login pogas esamĪbu
+  beforeEach(() => { // Cikls, kas atkārtojas pirms katrā testā
+      cy.visit('https://estudijas.liepu.lv/'); // Ietvars atver mājaslapu
+      // Lieks solis šajā sistēma - cy.get('#login > div:nth-child(4) > input').should('exist').click(); //Ietvars atrod ielogošanas pogu uz klikšķina uz to
+      // Lieks solis šajā sistēma - cy.url().should('include', '/loginpage url artibūts'); // Ietvars pārbaude saites korektumu
+      cy.get('#login_username').should('exist'); // Ietvars pārbauda lauka login esamību
+      cy.get('#login_password').should('exist'); // Ietvars pārbauda lauka parole esamību
+      cy.get('#login > :nth-child(4) > .btn').should('exist'); // Ietvars pārbauda login pogas esamĪbu
   })
 
   it('Login formas aizpildīšana', () => {
-      cy.get('#login_username').click().type('login'); //ietvars aizpilda login lauku ar loginu
-      cy.get('#login_password').click().type('parole'); //ietvars aizpilda paroles lauku ar paroli
-      cy.get('#login > :nth-child(4) > .btn').should('exist').click(); ////ietvars click "Log in"
-      cy.url().should('include', '/my/'); //lietotāja profila url pārbaude
+      cy.get('#login_username').click().type('login'); // Ietvars aizpilda login lauku ar login datiem
+      cy.get('#login_password').click().type('parole'); // Ietvars aizpilda paroles lauku ar paroles datiem
+      cy.get('#login > :nth-child(4) > .btn').should('exist').click(); // Ietvars click "Log in"
+      cy.url().should('include', '/my/'); // Lietotāja profila url pārbaude
   })
 
 })
