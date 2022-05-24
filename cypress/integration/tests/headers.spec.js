7787ffb69a36f511d824ec83ea68ab8e0f1e1e7d
Ksenia Kolyadina <ksenija.koladina@gmail.com>
2022-05-24 21:18:49 +0300

    fix

--- cypress/integration/tests/headers.spec.js
+++ cypress/integration/tests/headers.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-describe('Meklēšanas sistēma', () => {
+describe('Virsraksta pārbaude', () => {
   beforeEach(() => {
     cy.visit('https://liepu.lv/lv'); // ietvars apmeklē mājaslapu
   })
