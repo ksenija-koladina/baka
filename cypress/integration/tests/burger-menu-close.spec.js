357a6c48a803bbdd567bf18ce5dc99098cf0dac6
Ksenia Kolyadina <ksenija.koladina@gmail.com>
2022-05-25 01:54:20 +0300

    burger menu close fix

--- cypress/integration/tests/burger-menu-close.spec.js
+++ cypress/integration/tests/burger-menu-close.spec.js
@@ -2,14 +2,14 @@
 
 describe('Burger izvēlne - aizvērt', () => {
   beforeEach(() => {
-    cy.visit('Mājaslpas adrese'); // ietvars apmeklē mājaslapu
-    cy.get('Burger izvēlne').click() // ietvars klikšķina uz burger izvēlni un atvēr to
-    cy.get('Burger izvēlnes logs').should('be.visible'); // burger izvēlnes logs ir redzams
+    cy.visit('Mājaslpas adrese'); // Ietvars apmeklē mājaslapu
+    cy.get('Burger izvēlne').click() // Ietvars klikšķina uz burger izvēlni un atvēr to
+    cy.get('Burger izvēlnes logs').should('be.visible'); // Ietvārs pārbauda, vai burger izvēlnes logs ir redzams
   })
 
   it('Burger izvēlne aizvērās ar krustiņa klikšķi', () => {
-      cy.get('Krustiņš').click(); // ietvars klikšķina uz burger izvēlnes krustiņu (close poga) un aizvēr to
-      cy.get('Burger izvēlnes logs').should('not.be.visible'); // burger izvēlnes logs nav redzams
+      cy.get('Krustiņš').click(); // Ietvars klikšķina uz burger izvēlnes krustiņu (close poga) un aizvēr to
+      cy.get('Burger izvēlnes logs').should('not.be.visible'); // Ietvārs pārbauda, vai burger izvēlnes logs nav redzams
   })
 
 
