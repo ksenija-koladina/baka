ed27b69c9e4455448d331a5107c98967dfd467fe
Ksenia Kolyadina <ksenija.koladina@gmail.com>
2022-05-24 22:00:21 +0300

    burger menu close

--- cypress/integration/tests/burger-menu-close.spec.js
+++ cypress/integration/tests/burger-menu-close.spec.js
@@ -2,14 +2,14 @@
 
 describe('Burger izvēlne - aizvērt', () => {
   beforeEach(() => {
-    cy.visit('https://liepu.lv/lv'); // ietvars apmeklē mājaslapu
-    cy.get('body > header > a').click() // ietvars klikšķina uz burger izvēlni un atvēr to
-    cy.get('#menu > div').should('be.visible'); // burger izvēlnes logs ir redzams
+    cy.visit('Mājaslpas adrese'); // ietvars apmeklē mājaslapu
+    cy.get('Burger izvēlne').click() // ietvars klikšķina uz burger izvēlni un atvēr to
+    cy.get('').should('be.visible'); // burger izvēlnes logs ir redzams
   })
 
   it('Burger izvēlne aizvērās ar krustiņa klikšķi', () => {
-      cy.get('#menu > div > div.text-right.text-md-left > a').click(); // ietvars klikšķina uz burger izvēlnes krustiņu (close poga) un aizvēr to
-      cy.get('#menu > div').should('not.be.visible'); // burger izvēlnes logs nav redzams
+      cy.get('').click(); // ietvars klikšķina uz burger izvēlnes krustiņu (close poga) un aizvēr to
+      cy.get('').should('not.be.visible'); // burger izvēlnes logs nav redzams
   })
 
 
