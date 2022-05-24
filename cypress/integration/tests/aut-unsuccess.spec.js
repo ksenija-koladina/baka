c37d76c7d74eb2e9500d293200b3c13366afa541
Ksenia Kolyadina <ksenija.koladina@gmail.com>
2022-05-25 01:52:53 +0300

    aut unsuccess fix

--- cypress/integration/tests/aut-unsuccess.spec.js
+++ cypress/integration/tests/aut-unsuccess.spec.js
@@ -1,34 +1,34 @@
 /// <reference types="cypress" />
 
 describe('Autorizēšanas formas testēšana - neveiksmīga', () => {
-  beforeEach(() => { //cikls, kas atkārtojas pirms katrā testā
+  beforeEach(() => { // Cikls, kas atkārtojas pirms katrā testā
       cy.visit('https://estudijas.liepu.lv/'); // ietvars atver mājaslapu
-      // lieks solis šajā sistēma - cy.get('#login > div:nth-child(4) > input').should('exist').click(); //ietvars atrod ielogošanas pogu uz klikšķina uz to
-      // lieks solis šajā sistēma - cy.url().should('include', '/loginpage url artibūts'); // ietvars pārbaude linka korektumu
-      cy.get('#login_username').should('exist'); // ietvars pārbauda lauka login esamĪbu
-      cy.get('#login_password').should('exist'); // ietvars pārbauda lauka parole esamĪbu
-      cy.get('#login > :nth-child(4) > .btn').should('exist'); // ietvars pārbauda login pogas esamĪbu
+      // Lieks solis šajā sistēmā - cy.get('#login > div:nth-child(4) > input').should('exist').click(); // Ietvars atrod ielogošanas pogu un klikšķina uz to
+      // Lieks solis šajā sistēmā - cy.url().should('include', '/loginpage url artibūts'); // Ietvars pārbaude linka korektumu
+      cy.get('#login_username').should('exist'); // Ietvars pārbauda lauka "Login" esamību
+      cy.get('#login_password').should('exist'); // Ietvars pārbauda lauka "Parole" esamību
+      cy.get('#login > :nth-child(4) > .btn').should('exist'); // Ietvars pārbauda "Log in" pogas esamību
   })
 
   it('Login formas aizpildīšana - kļūdains login', () => {
-      cy.get('#login_username').click().type('login'); //ietvars aizpilda login lauku ar loginu (kļūdaini)
-      cy.get('#login_password').click().type('parole'); //ietvars aizpilda paroles lauku ar paroli
-      cy.get('#login > :nth-child(4) > .btn').should('exist').click(); //ietvars click "Log in"
-      cy.get('#region-main > div > div:nth-child(3) > div > div > div > div > div.loginerrors.mt-3 > div').should('exist'); // ietvars pārbauda kļūdas ziņojuma esamĪbu
+      cy.get('#login_username').click().type('login'); // Ietvars aizpilda "Login" lauku ar login datiem (kļūdaini)
+      cy.get('#login_password').click().type('parole'); // Ietvars aizpilda "Paroles" lauku ar paroles datiem
+      cy.get('#login > :nth-child(4) > .btn').should('exist').click(); // Ietvars klikšķina "Log in"
+      cy.get('#region-main > div > div:nth-child(3) > div > div > div > div > div.loginerrors.mt-3 > div').should('exist'); // Ietvars pārbauda kļūdas ziņojuma esamību
   })
 
   it('Login formas aizpildīšana - kļūdaina parole', () => {
-    cy.get('#login_username').click().type('login'); //ietvars aizpilda login lauku ar loginu
-    cy.get('#login_password').click().type('parole'); //ietvars aizpilda paroles lauku ar paroli (kļūdaini)
-    cy.get('#login > :nth-child(4) > .btn').should('exist').click(); //ietvars click "Log in"
-    cy.get('#region-main > div > div:nth-child(3) > div > div > div > div > div.loginerrors.mt-3 > div').should('exist'); // ietvars pārbauda kļūdas ziņojuma esamĪbu
+    cy.get('#login_username').click().type('login'); // Ietvars aizpilda login lauku ar login datiem
+    cy.get('#login_password').click().type('parole'); // Ietvars aizpilda paroles lauku ar paroles datiem (kļūdaini)
+    cy.get('#login > :nth-child(4) > .btn').should('exist').click(); // Ietvars klikšķina "Log in"
+    cy.get('#region-main > div > div:nth-child(3) > div > div > div > div > div.loginerrors.mt-3 > div').should('exist'); // Ietvars pārbauda kļūdas ziņojuma esamību
   })
 
   it('Login formas aizpildīšana - abi lauki ir aizpildīti ar nevalīdiem datiem', () => {
-    cy.get('#login_username').click().type('login'); //ietvars aizpilda login lauku ar loginu (kļūdaini)
-    cy.get('#login_password').click().type('parole'); //ietvars aizpilda paroles lauku ar paroli (kļūdaini)
-    cy.get('#login > :nth-child(4) > .btn').should('exist').click(); //ietvars click "Log in"
-    cy.get('#region-main > div > div:nth-child(3) > div > div > div > div > div.loginerrors.mt-3 > div').should('exist'); // ietvars pārbauda kļūdas ziņojuma esamĪbu
+    cy.get('#login_username').click().type('login'); // Ietvars aizpilda login lauku ar login datiem(kļūdaini)
+    cy.get('#login_password').click().type('parole'); // Ietvars aizpilda paroles lauku ar paroles datiem(kļūdaini)
+    cy.get('#login > :nth-child(4) > .btn').should('exist').click(); // Ietvars klikšķina "Log in"
+    cy.get('#region-main > div > div:nth-child(3) > div > div > div > div > div.loginerrors.mt-3 > div').should('exist'); // Ietvars pārbauda kļūdas ziņojuma esamību
   })
 
 })
