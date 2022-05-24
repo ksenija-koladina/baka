07763136749103e0adf304230690128a6c45c3a5
Ksenia Kolyadina <ksenija.koladina@gmail.com>
2022-05-25 01:42:47 +0300

    burger changes

--- cypress/integration/tests/burger-menu-close.spec.js
+++ cypress/integration/tests/burger-menu-close.spec.js
@@ -4,12 +4,12 @@ describe('Burger izvēlne - aizvērt', () => {
   beforeEach(() => {
     cy.visit('Mājaslpas adrese'); // ietvars apmeklē mājaslapu
     cy.get('Burger izvēlne').click() // ietvars klikšķina uz burger izvēlni un atvēr to
-    cy.get('').should('be.visible'); // burger izvēlnes logs ir redzams
+    cy.get('Burger izvēlnes logs').should('be.visible'); // burger izvēlnes logs ir redzams
   })
 
   it('Burger izvēlne aizvērās ar krustiņa klikšķi', () => {
-      cy.get('').click(); // ietvars klikšķina uz burger izvēlnes krustiņu (close poga) un aizvēr to
-      cy.get('').should('not.be.visible'); // burger izvēlnes logs nav redzams
+      cy.get('Krustiņš').click(); // ietvars klikšķina uz burger izvēlnes krustiņu (close poga) un aizvēr to
+      cy.get('Burger izvēlnes logs').should('not.be.visible'); // burger izvēlnes logs nav redzams
   })
 
 
--- cypress/integration/tests/burger-menu-links.spec.js
+++ cypress/integration/tests/burger-menu-links.spec.js
@@ -2,16 +2,16 @@
 
 describe('Burger izvēlnes saites', () => {
   beforeEach(() => {
-    cy.visit(''); // ietvars apmeklē mājaslapu
-    cy.get('').click() // ietvars klikšķina uz burger menu un atvēr to
-    cy.get('').should('be.visible'); // burger izvēlnes logs ir redzams
+    cy.visit('Mājaslapas adrese'); // ietvars apmeklē mājaslapu
+    cy.get('Burger izvēlne').click() // ietvars klikšķina uz burger menu un atvēr to
+    cy.get('Burger izvēlnes logs').should('be.visible'); // burger izvēlnes logs ir redzams
   })
 
   it('Burger izvēlnes saites atvēr vajadzīgo lapu un saturs atbilst saitei', () => {
-      cy.get('').should('be.visible'); // izvēlnē rādas saraksta elementi
-      cy.get('').click(); //klikšķis uz elementu 
-      cy.url().should('include', '/'); //saites pārbaude (vai notika navigācija)
-      cy.get('').should('contain.text', 'Projekti'); //virsraksta pārbaude
+      cy.get('Burger izvēlnes elements').should('be.visible'); // izvēlnē rādas saraksta elementi
+      cy.get('Burger izvēlnes elements').click(); //klikšķis uz elementu 
+      cy.url().should('include', '/saites daļa'); //saites pārbaude (vai notika navigācija)
+      cy.get('Virsraksts').should('contain.text', 'Virsraksta nosaukums'); //virsraksta pārbaude
   })
 
 
