c72f85fda820cc7261a44b7e4a19b15ef783ed4a
Ksenia Kolyadina <ksenija.koladina@gmail.com>
2022-05-25 13:39:11 +0300

    main aut unsuccess spec fix

--- cypress/integration/tests/aut-unsuccess.spec.js
+++ cypress/integration/tests/aut-unsuccess.spec.js
@@ -1,34 +1,34 @@
 /// <reference types="cypress" />
 
 describe('Autorizēšanas formas testēšana - neveiksmīga', () => {
-  beforeEach(() => { //cikls, kas atkārtojas pirms katrā testā
-      cy.visit('mājaslpas adrese'); // ietvars atver mājaslapu
-      cy.get('Sign in poga').should('exist').click(); //ietvars atrod ielogošanas pogu uz klikšķina uz to
-      cy.url().should('include', '/loginpage url artibūts'); // ietvars pārbaude linka korektumu
-      cy.get('login lauks').should('exist'); // ietvars pārbauda lauka login esamĪbu
-      cy.get('paroles lauks').should('exist'); // ietvars pārbauda lauka parole esamĪbu
-      cy.get('log in poga').should('exist'); // ietvars pārbauda login pogas esamĪbu
+  beforeEach(() => { // Cikls, kas atkārtojas pirms katrā testā
+      cy.visit('Mājaslpas adrese'); // Ietvars atver mājaslapu
+      cy.get('Sign in poga').should('exist').click(); // Ietvars atrod ielogošanas pogu un klikšķina uz to
+      cy.url().should('include', '/loginpage url artibūts'); // Ietvars pārbaude saites korektumu
+      cy.get('Login lauks').should('exist'); // Ietvars pārbauda lauka "Login" esamību
+      cy.get('Paroles lauks').should('exist'); // Ietvars pārbauda lauka "Parole" esamību
+      cy.get('Log in poga').should('exist'); // Ietvars pārbauda "Log in" pogas esamību
   })
 
   it('Login formas aizpildīšana - kļūdains login', () => {
-      cy.get('login lauks').click().type('login'); //ietvars aizpilda login lauku ar loginu (kļūdaini)
-      cy.get('paroles lauks').click().type('parole'); //ietvars aizpilda paroles lauku ar paroli
-      cy.get('log in poga').should('exist').click(); //ietvars click "Log in"
-      cy.get('error message').should('exist'); // ietvars pārbauda kļūdas ziņojuma esamĪbu
+      cy.get('Login lauks').click().type('login'); // Ietvars aizpilda "Login" lauku ar login datiem (kļūdaini)
+      cy.get('Paroles lauks').click().type('parole'); // Ietvars aizpilda "Paroles" lauku ar paroles datiem
+      cy.get('Log in poga').should('exist').click(); // Ietvars klikšķina "Log in" pogu
+      cy.get('Error message').should('exist'); // // Ietvars pārbauda kļūdas ziņojuma esamību
   })
 
   it('Login formas aizpildīšana - kļūdaina parole', () => {
-    cy.get('login lauks').click().type('login'); //ietvars aizpilda login lauku ar loginu
-    cy.get('paroles lauks').click().type('parole'); //ietvars aizpilda paroles lauku ar paroli (kļūdaini)
-    cy.get('log in poga').should('exist').click(); //ietvars click "Log in"
-    cy.get('error message').should('exist'); // ietvars pārbauda kļūdas ziņojuma esamĪbu
+    cy.get('Login lauks').click().type('login'); // Ietvars aizpilda "Login" lauku ar login datiem
+    cy.get('Paroles lauks').click().type('parole'); // Ietvars aizpilda "Paroles" lauku ar paroles datiem (kļūdaini)
+    cy.get('Log in poga').should('exist').click(); // Ietvars klikšķina "Log in" pogu
+    cy.get('Error message').should('exist'); // // Ietvars pārbauda kļūdas ziņojuma esamību
   })
 
   it('Login formas aizpildīšana - abi lauki ir aizpildīti ar nevalīdiem datiem', () => {
-    cy.get('login lauks').click().type('login'); //ietvars aizpilda login lauku ar loginu (kļūdaini)
-    cy.get('paroles lauks').click().type('parole'); //ietvars aizpilda paroles lauku ar paroli (kļūdaini)
-    cy.get('log in poga').should('exist').click(); //ietvars click "Log in"
-    cy.get('error message').should('exist'); // ietvars pārbauda kļūdas ziņojuma esamĪbu
+    cy.get('Login lauks').click().type('login'); // Ietvars aizpilda "Login" lauku ar login datiem (kļūdaini)
+    cy.get('Paroles lauks').click().type('parole'); // Ietvars aizpilda "Paroles" lauku ar paroles datiem (kļūdaini)
+    cy.get('Log in poga').should('exist').click(); // Ietvars klikšķina "Log in" pogu
+    cy.get('Error message').should('exist'); // // Ietvars pārbauda kļūdas ziņojuma esamību
   })
 
 })
