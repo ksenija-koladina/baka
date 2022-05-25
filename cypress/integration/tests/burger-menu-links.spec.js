fd8ef5e8c0ab42666af3c5b95364f677ef627fe6
Ksenia Kolyadina <ksenija.koladina@gmail.com>
2022-05-25 13:44:01 +0300

    main burger menu links spec fix

--- cypress/integration/tests/burger-menu-links.spec.js
+++ cypress/integration/tests/burger-menu-links.spec.js
@@ -2,16 +2,16 @@
 
 describe('Burger izvēlne - aizvērt', () => {
   beforeEach(() => {
-    cy.visit('mājaslapas adrese'); // ietvars apmeklē mājaslapu
-    cy.get('burger izvēlne').click() // ietvars klikšķina uz burger menu un atvēr to
-    cy.get('burger izvēlnes logs').should('be.visible'); // burger izvēlnes logs ir redzams
+    cy.visit('Mājaslapas adrese'); // Ietvars apmeklē mājaslapu
+    cy.get('Burger izvēlne').click() // Ietvars klikšķina uz burger izvēlnes un atver to
+    cy.get('Burger izvēlnes logs').should('be.visible'); // Burger izvēlnes logs ir redzams
   })
 
   it('Burger izvēlnē ir saites un tie naviģē uz nepieciešamo lapu', () => {
-      cy.get('izvēlnes saraksts').should('be.visible'); // izvēlnē rādas saraksta elementi
-      cy.get('saraksta elements').click(); //klikšķis uz elementu 
-      cy.url().should('include', '/linka daļa'); //saites pārbaude (vai notika navigācija)
-      cy.get('virsraksts').should('contain.text', 'Virsraksta teksts'); //virsraksta pārbaude
+      cy.get('Izvēlnes saraksts').should('be.visible'); // Izvēlnē rādās saraksta elementi
+      cy.get('Saraksta elements').click(); // Klikšķis uz elementu  
+      cy.url().should('include', '/saites daļa'); // Saites pārbaude (vai notika navigācija)
+      cy.get('Virsraksts').should('contain.text', 'Virsraksta teksts'); // Virsraksta pārbaude
   })
 
 
