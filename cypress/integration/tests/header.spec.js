31c275b73dd1e51668e5010360ddf5bd6a561be1
Ksenia Kolyadina <ksenija.koladina@gmail.com>
2022-05-25 14:51:04 +0300

    main header spec fix

--- cypress/integration/tests/header.spec.js
+++ cypress/integration/tests/header.spec.js
@@ -2,12 +2,12 @@
 
 describe('Virsrakstu pārbaude', () => {
   beforeEach(() => {
-    cy.visit('Mājaslapas adrese'); // ietvars apmeklē mājaslapu
+    cy.visit('Mājaslapas adrese'); // Ietvars apmeklē mājaslapu
   })
 
   it('Virsraksta nosaukums ir pareizs', () => {
-      cy.get('Virsraksts').should('exist'); // ietvars pārbauda, vai mājaslapai ir virsraksts
-      cy.get('Virsraksts').should('contain.text', 'Virsraksta nosaukums'); //ietvars pārbauda, vai virsraksts atbilst gaidāmai vērtībai
+      cy.get('Virsraksts').should('exist'); // Ietvars pārbauda, vai mājaslapai ir virsraksts
+      cy.get('Virsraksts').should('contain.text', 'Virsraksta nosaukums'); // Ietvars pārbauda, vai virsraksts atbilst gaidāmai vērtībai
   })
 
 })
