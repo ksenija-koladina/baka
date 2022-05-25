5e764b17e8f726a6b421218b81885ee642fa5528
Ksenia Kolyadina <ksenija.koladina@gmail.com>
2022-05-25 14:52:34 +0300

    main search spec fix

--- cypress/integration/tests/search.spec.js
+++ cypress/integration/tests/search.spec.js
@@ -2,15 +2,15 @@
 
 describe('Meklēšanas sistēma', () => {
   beforeEach(() => {
-    cy.visit('Mājaslapas adrese'); // ietvars apmeklē mājaslapu
+    cy.visit('Mājaslapas adrese'); // Ietvars apmeklē mājaslapu
   })
 
   it('Meklēšanas sistēmas pārbaude', () => {
-      cy.get('Meklēšanas ievades laukums').click(); // ietvars klikšķina uz meklešanas ievadlauku
-      cy.get('Meklēšanas ievades laukums').type('Meklējamais vārds vai teikums'); //ietvars ievada meklējamo vardu
-      cy.get('Meklēšanas poga').click(); //ietvars noklikšķina pogu "meklēt"
-      cy.get('Meklēšanas rezultāts').should('exist'); // ietvars pārbauda, vai ir paradijušas testa rezultāti
-      cy.get('Meklēšanas rezultāts').contains('Meklējamais vārds vai teikums'); // ietvars pārbauda, vai meklēšanas rezultātā ir meklējamais vards
+      cy.get('Meklēšanas ievades laukums').click(); // Ietvars klikšķina uz meklēšanas  joslu
+      cy.get('Meklēšanas ievades laukums').type('Meklējamais vārds vai teikums'); // Ietvars ievada meklējamo vārdu
+      cy.get('Meklēšanas poga').click(); // Ietvars noklikšķina pogu "Meklēt"
+      cy.get('Meklēšanas rezultāts').should('exist'); // Ietvars pārbauda, vai ir paradījušās meklēšanas rezultāti
+      cy.get('Meklēšanas rezultāts').contains('Meklējamais vārds vai teikums'); // Ietvars pārbauda, vai meklēšanas rezultātā ir meklējamais vārds
   })
 
 })
