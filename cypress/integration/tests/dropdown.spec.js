aa02ba2ab1b0d0cfe9e6a8e23a8f217e9cb7e550
Ksenia Kolyadina <ksenija.koladina@gmail.com>
2022-05-25 14:50:07 +0300

    main dropdown spec fix

--- cypress/integration/tests/dropdown.spec.js
+++ cypress/integration/tests/dropdown.spec.js
@@ -2,16 +2,16 @@
 
 describe('Dropdown izvēlne', () => {
   beforeEach(() => {
-    cy.visit('Mājaslapas adrese'); // ietvars apmeklē mājaslapu
+    cy.visit('Mājaslapas adrese'); // Ietvars apmeklē mājaslapu
   })
 
   it('Dropdown izvēlnes saites atvēr vajadzīgo lapu un saturs atbilst saitei', () => {
-      cy.get('Dropdown izvēlne').should('be.visible'); // ietvars pārbauda dropdown izvelni
-      cy.get('Dropdown izvēlne').click(); //ietvars klikšķina uz dropdown izvēlni 
-      cy.get('Dropdown izvēlnes elements').should('be.visible'); //izvelnes saraksta elementi ir redzami
-      cy.get('Dropdown izvēlne elements').click(); //ietvars klikšķina uz dropdown izvelnes saraksta elementu
-      cy.url().should('include', '/saites daļa'); //saites pārbaude (vai notika navigācija)
-      cy.get('Virsraksts').should('contain.text', 'Virsraksta nosaukums'); //virsraksta pārbaude
+      cy.get('Dropdown izvēlne').should('be.visible'); // Ietvars pārbauda dropdown izvēlnes esamību
+      cy.get('Dropdown izvēlne').click(); // Ietvars klikšķina uz dropdown izvēlni 
+      cy.get('Dropdown izvēlnes elements').should('be.visible'); // Izvēlnes saraksta elementi ir redzami
+      cy.get('Dropdown izvēlne elements').click(); // Ietvars klikšķina uz dropdown izvēlnes saraksta elementu
+      cy.url().should('include', '/saites daļa'); // Saites pārbaude (vai notika navigācija)
+      cy.get('Virsraksts').should('contain.text', 'Virsraksta nosaukums'); // Virsraksta pārbaude
   })
 
 })
